d8a34a93f5f918be5b96b1dc50425a5216c6dfd3
wtsay <wtsay@dvrpc.org>
2017-12-15 14:44:43 -0500

    Delaunay+Voronoi Desire Lines
    I'm rather proud of this one

--- api/qry/_init_desirelines.sql
+++ api/qry/_init_desirelines.sql
@@ -136,15 +136,20 @@ SELECT *,
 ) _q
 
 WITH zone_index AS (SELECT indexp1 - 1 AS index, no FROM (SELECT row_number() OVER (ORDER BY no) AS indexp1, no FROM net_zones) _q)
+SELECT _q0.*, n.geom FROM (
+SELECT _q1.edge, SUM(val) totalval
+FROM (
 SELECT fn.*, z.no, zi.index, mtx_2000_am.val
 FROM pgr_dijkstra(
-    'SELECT id, source, target, cost AS cost, rcost as reverse_cost FROM _debug_net',
-    16266,
-    (SELECT array_agg(DISTINCT(target)) FROM _debug_net WHERE dzone <> 16266),
-    directed := true
+	'SELECT id, source, target, cost AS cost, rcost as reverse_cost FROM _debug_net',
+	16266,
+	(SELECT array_agg(DISTINCT(target)) FROM _debug_net WHERE dzone <> 16266),
+	directed := true
 ) fn
-LEFT JOIN _debug_net n ON fn.edge = n.id
 LEFT JOIN _debug_network_zones z ON z.id = fn.end_vid
 LEFT JOIN zone_index zi ON z.no = zi.no
 LEFT JOIN mtx_2000_am ON oindex = 134 AND dindex = zi.index
-ORDER BY seq
+) _q1
+GROUP BY _q1.edge
+) _q0
+LEFT JOIN _debug_net n ON _q0.edge = n.id
