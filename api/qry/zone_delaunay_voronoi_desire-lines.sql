e7a3d7b21aef49ac2638c005c54bea3ffe08df5f
wtsay <wtsay@dvrpc.org>
2017-12-15 15:45:51 -0500

    Cleaned up

--- api/qry/zone_delaunay_voronoi_desire-lines.sql
+++ api/qry/zone_delaunay_voronoi_desire-lines.sql
@@ -39,20 +39,19 @@ FROM (
 ) _q
 
 WITH zone_index AS (SELECT indexp1 - 1 AS index, no FROM (SELECT row_number() OVER (ORDER BY no) AS indexp1, no FROM net_zones) _q)
-SELECT _q0.*, n.geom FROM (
-SELECT _q1.edge, SUM(val) totalval
-FROM (
-SELECT fn.*, z.no, zi.index, mtx_2000_am.val
+SELECT sp.*, net.geom FROM (
+SELECT
+    fn.edge,
+    SUM(val) totalval
 FROM pgr_dijkstra(
-	'SELECT id, source, target, cost AS cost, rcost as reverse_cost FROM _debug_net',
-	16266,
-	(SELECT array_agg(DISTINCT(target)) FROM _debug_net WHERE dzone <> 16266),
-	directed := true
+    'SELECT id, source, target, cost AS cost, rcost as reverse_cost FROM _debug_net',
+    16266,
+    (SELECT array_agg(id) FROM _debug_network_zones WHERE no <> 135),
+    directed := true
 ) fn
 LEFT JOIN _debug_network_zones z ON z.id = fn.end_vid
 LEFT JOIN zone_index zi ON z.no = zi.no
-LEFT JOIN mtx_2000_am ON oindex = 134 AND dindex = zi.index
-) _q1
-GROUP BY _q1.edge
-) _q0
-LEFT JOIN _debug_net n ON _q0.edge = n.id
+LEFT JOIN mtx_2000_am mtx ON mtx.oindex = 134 AND mtx.dindex = zi.index
+GROUP BY fn.edge
+) sp
+LEFT JOIN _debug_network net ON net.id = sp.edge
