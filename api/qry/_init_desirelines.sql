118e5255aac1886af62d0b7198709c6fa90243d0
wtsay <wtsay@dvrpc.org>
2017-12-15 12:19:22 -0500

    Delaunay + Voronoi Desire Lines

--- api/qry/_init_desirelines.sql
+++ api/qry/_init_desirelines.sql
@@ -5,6 +5,7 @@ DROP TABLE IF EXISTS _debug_network_vertices_pgr;
 
 CREATE TABLE _debug_delaunay AS
 SELECT
+    (row_number() OVER ())::integer id,
     oz.no ozone,
     dz.no dzone,
     st_length(_q2.geom) length,
@@ -34,7 +35,7 @@ CREATE INDEX IF NOT EXISTS _debug_delaunay_gidx ON _debug_delaunay USING GIST (g
 
 CREATE TABLE _debug_voronoi AS
 SELECT
-    (row_number() OVER ())::integer AS id,
+    (row_number() OVER ())::integer id,
     (geodump).geom,
     NULL::integer source,
     NULL::integer target
@@ -47,9 +48,12 @@ CREATE INDEX IF NOT EXISTS _debug_voronoi_gidx ON _debug_voronoi USING GIST (geo
 CREATE TABLE _debug_network AS
 SELECT
     (row_number() OVER ())::integer id,
-    *
+    *,
+    NULL::integer ozone,
+    NULL::integer dzone
 FROM (
     WITH
+    -- Delaunay cut by Voronoi
     DxV AS (
         SELECT d.id did, ST_Dump(ST_Split(d.geom, _d.geom)) geodump
         FROM _debug_delaunay d
@@ -61,6 +65,7 @@ FROM (
             GROUP BY d.id
         )_d ON d.id = _d.did
     ),
+    -- Voronoi cut by Delaunay
     VxD AS (
         SELECT v.id vid, ST_Dump(ST_Split(v.geom, _v.geom)) geodump
         FROM _debug_voronoi v
@@ -80,6 +85,47 @@ FROM (
 ) _q;
 CREATE INDEX IF NOT EXISTS _debug_network_gidx ON _debug_network USING GIST (geom);
 
-SELECT pgr_createTopology('_debug_network', 0.0001, 'geom', 'id');
+SELECT pgr_createTopology('_debug_network', 0.000001, 'geom', 'id');
 
-SELECT COUNT(*) FROM _debug_network_vertices_pgr;
+-- Update network table with zone identifiers (if applicable)
+UPDATE _debug_network
+SET ozone = z.no
+FROM _debug_network_vertices_pgr n JOIN geom_zones z ON z.wktloc = n.the_geom
+WHERE source = n.id;
+
+UPDATE _debug_network
+SET dzone = z.no
+FROM _debug_network_vertices_pgr n JOIN geom_zones z ON z.wktloc = n.the_geom
+WHERE target = n.id;
+
+-- STATIC
+-- 
+SELECT *, 
+    (tflag * length) + (o_out * length) + (d_in * length) AS cost,
+    (tflag * length) + (o_in * length) + (d_out * length) AS rcost
+FROM (
+SELECT *,
+    ST_Length(geom) length,
+    (ozone IS NULL AND dzone IS NULL)::integer tflag, -- Trunk Flag (always reverseable)
+    CASE
+        WHEN ozone = 135 THEN 1
+        WHEN dzone = 135 THEN 999999
+        ELSE 0
+    END o_out,
+    CASE
+        WHEN dzone = 135 THEN 1
+        WHEN ozone = 135 THEN 999999
+        ELSE 0
+    END o_in,
+    CASE 
+        WHEN ozone = 135 THEN 0
+        WHEN ozone IS NOT NULL THEN 1
+        ELSE 999999
+    END d_out,
+    CASE 
+        WHEN dzone = 135 THEN 0
+        WHEN dzone IS NOT NULL THEN 1
+        ELSE 999999
+    END d_in
+    FROM _debug_network
+) _q ORDER BY o_in DESC
