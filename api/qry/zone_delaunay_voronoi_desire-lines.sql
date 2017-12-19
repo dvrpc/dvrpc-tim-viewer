bd1ef99ed3017b45f1782492e3d6b4547e27e1f0
wtsay <wtsay@dvrpc.org>
2017-12-19 16:07:48 -0500

    Solar Ohm
    Merkaba

--- api/qry/zone_delaunay_voronoi_desire-lines.sql
+++ api/qry/zone_delaunay_voronoi_desire-lines.sql
@@ -1,50 +1,44 @@
-zoneno
-zoneindex
-zonevid
-
--- STATIC (Function zone)
--- 
-CREATE TEMPORARY TABLE 
-SELECT id, source, target,
-    fwd * length AS cost,
-    rev * length AS rcost
-FROM (
-    SELECT
-        id, source, target,
-        ST_Length(geom) length,
-        (ozone IS NULL AND dzone IS NULL)::integer tflag, -- Trunk Flag (always reverseable)
-    CASE
-        WHEN ozone IS NULL AND dzone IS NULL THEN 1
-        WHEN ozone IS NULL THEN CASE
-            WHEN dzone = 135 THEN 999999
-            ELSE 1
-        END
-        ELSE CASE
-            WHEN ozone = 135 THEN 1
-            ELSE 999999
-        END
-    END fwd,
-    CASE
-        WHEN dzone IS NULL AND ozone IS NULL THEN 1
-        WHEN dzone IS NULL THEN CASE
-            WHEN ozone = 135 THEN 999999
-            ELSE 1
-        END
-        ELSE CASE
-            WHEN dzone = 135 THEN 1
-            ELSE 999999
-        END
-        END rev
-    FROM _debug_network
-) _q
-
 WITH zone_index AS (SELECT indexp1 - 1 AS index, no FROM (SELECT row_number() OVER (ORDER BY no) AS indexp1, no FROM net_zones) _q)
 SELECT sp.*, net.geom FROM (
 SELECT
     fn.edge,
     SUM(val) totalval
 FROM pgr_dijkstra(
-    'SELECT id, source, target, cost AS cost, rcost as reverse_cost FROM _debug_net',
+    'SELECT id, source, target, cost AS cost, rcost as reverse_cost
+    FROM (
+        SELECT id, source, target,
+            fwd * length AS cost,
+            rev * length AS rcost
+        FROM (
+            SELECT
+                id, source, target,
+                ST_Length(geom) length,
+                (ozone IS NULL AND dzone IS NULL)::integer tflag, -- Trunk Flag (always reverseable)
+            CASE
+                WHEN ozone IS NULL AND dzone IS NULL THEN 1
+                WHEN ozone IS NULL THEN CASE
+                    WHEN dzone = 135 THEN 999999
+                    ELSE 1
+                END
+                ELSE CASE
+                    WHEN ozone = 135 THEN 1
+                    ELSE 999999
+                END
+            END fwd,
+            CASE
+                WHEN dzone IS NULL AND ozone IS NULL THEN 1
+                WHEN dzone IS NULL THEN CASE
+                    WHEN ozone = 135 THEN 999999
+                    ELSE 1
+                END
+                ELSE CASE
+                    WHEN dzone = 135 THEN 1
+                    ELSE 999999
+                END
+                END rev
+            FROM _debug_network
+        ) _q
+    ) _q',
     16266,
     (SELECT array_agg(id) FROM _debug_network_zones WHERE no <> 135),
     directed := true
