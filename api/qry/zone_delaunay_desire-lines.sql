c088300b439bc1f7ad8b96b85fdf8da64e9fc81e
wtsay <wtsay@dvrpc.org>
2017-12-19 14:41:40 -0500

    I think this works?
    Issue with QGIS:
    QGIS in the database manager will use read/write transactions.
    QGIS when displaying the data 'normally', i.e. already added to the
    layers, it will revert to read-only which breaks the fucking function.

--- api/qry/zone_delaunay_desire-lines.sql
+++ api/qry/zone_delaunay_desire-lines.sql
@@ -8,16 +8,16 @@ RETURNS TABLE (
     totalval DOUBLE PRECISION,
     ozone INTEGER,
     dzone INTEGER,
-    geom GEOMETRY
+    geom GEOMETRY(LINESTRING, 4326)
 )  AS $$
 DECLARE
     origzoneindex INTEGER;
     destzoneindices INTEGER[];
-    dzno INTEGER;
     _tblname_mtx_am TEXT;
     _tblname_mtx_md TEXT;
     _tblname_mtx_pm TEXT;
     _tblname_mtx_nt TEXT;
+    dzno INTEGER;
 BEGIN
     _tblname_mtx_am := format('%I', 'mtx_' || mtxno || '_am');
     _tblname_mtx_md := format('%I', 'mtx_' || mtxno || '_md');
@@ -26,25 +26,36 @@ BEGIN
 
     DROP TABLE IF EXISTS _destzone;
     DROP TABLE IF EXISTS _mtx_am;
+    DROP TABLE IF EXISTS _mtx_md;
+    DROP TABLE IF EXISTS _mtx_pm;
+    DROP TABLE IF EXISTS _mtx_nt;
+
     CREATE TEMPORARY TABLE _destzone (
         zoneindex INTEGER,
         zoneno INTEGER
     );
-    CREATE TEMPORARY TABLE _test (
+    CREATE TEMPORARY TABLE _mtx_am (
         oindex INTEGER, dindex INTEGER, val DOUBLE PRECISION
     );
+
     SELECT tim_getzoneindex(origzoneno) INTO origzoneindex;
     FOREACH dzno IN ARRAY destzonenos LOOP
         INSERT INTO _destzone VALUES (tim_getzoneindex(dzno), dzno);
     END LOOP;
     SELECT array_agg(_destzone.zoneindex) INTO destzoneindices FROM _destzone;
 
-    EXECUTE FORMAT('INSERT INTO _test SELECT * FROM %I WHERE oindex = $1 AND dindex IN (SELECT DISTINCT(zoneindex) FROM _destzone);', _mtx_am) USING origzoneindex;
+    EXECUTE FORMAT('
+        INSERT INTO _mtx_am
+        SELECT *
+        FROM %I
+        WHERE oindex = $1
+        AND dindex IN (SELECT DISTINCT(zoneindex) FROM _destzone);
+        ', _tblname_mtx_am) USING origzoneindex;
 
     RETURN QUERY WITH
-    _mtx1 AS (
+    _mtx AS (
         SELECT mtx.*, dz.zoneno AS dno
-        FROM _test mtx
+        FROM _mtx_am mtx
         LEFT JOIN _destzone dz ON dz.zoneindex = dindex
     )
     SELECT
@@ -52,20 +63,20 @@ BEGIN
         _q.totalval,
         d.ozone,
         d.dzone,
-        d.geom
+        d.geom::GEOMETRY(LINESTRING, 4326)
     FROM (
         SELECT fn.edge, SUM(val) totalval
         FROM pgr_dijkstra(
             'SELECT id, ozone source, dzone target, length AS cost, length as reverse_cost FROM _debug_delaunay',
-            origzoneindex,
-            destzoneindices,
+            origzoneno,
+            destzonenos,
             directed := false
         ) fn
-        LEFT JOIN _mtx1 ON _mtx1.dno = fn.end_vid
+        LEFT JOIN _mtx ON _mtx.dno = fn.end_vid
         GROUP BY fn.edge
     ) _q
     LEFT JOIN _debug_delaunay d
     ON d.id = _q.edge
-    WHERE _q.totalval IS NOT NULL;
+    WHERE d.geom IS NOT NULL;
 END;
 $$ LANGUAGE plpgsql;
