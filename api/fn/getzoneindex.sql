a18599b2cfb7e10fe038697dc606042a2804af20
wtsay <wtsay@dvrpc.org>
2017-12-19 14:05:09 -0500

    Bug riddled but it runs

--- api/fn/getzoneindex.sql
+++ api/fn/getzoneindex.sql
@@ -3,13 +3,13 @@ RETURNS integer AS $$
 DECLARE
     zoneindex INTEGER;
 BEGIN
-    CREATE TEMPORARY TABLE no_index (_zoneindex INTEGER, _zoneno INTEGER) ON COMMIT DROP;
-    INSERT INTO no_index SELECT (rownum - 1)::INTEGER zoneindex, no zoneno FROM (
-        SELECT row_number() OVER (ORDER BY no NULLS LAST) rownum, no FROM tim23_zone ORDER BY no
-    ) _q;
-    CREATE INDEX no_index_zoneindex_idx ON no_index (_zoneindex ASC NULLS LAST);
-    CREATE INDEX no_index_zoneno_idx ON no_index (_zoneno ASC NULLS LAST);
-    SELECT ni._zoneindex INTO zoneindex FROM no_index ni WHERE ni._zoneno = zoneno;
+    SELECT
+        indexp1 - 1 INTO zoneindex
+    FROM (
+        SELECT row_number() OVER (ORDER BY no) AS indexp1, *
+        FROM net_zones
+    ) _q
+    WHERE _q.no = zoneno;
     RETURN zoneindex;
 END;
 $$ LANGUAGE plpgsql;
