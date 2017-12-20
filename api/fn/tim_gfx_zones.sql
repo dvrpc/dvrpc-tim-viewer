a1868b9633d6513d59211eb8c72399d5693bce1f
wtsay <wtsay@dvrpc.org>
2017-12-20 13:14:29 -0500

    Zones: GeoJSON (Point)

--- api/fn/tim_gfx_zones.sql
+++ api/fn/tim_gfx_zones.sql
@@ -1,22 +1,23 @@
 CREATE OR REPLACE FUNCTION tim_gfx_zones()
-RETURNS json AS $$
+RETURNS JSON AS $$
 DECLARE
     geojson JSON;
 BEGIN
-    
-SELECT row_to_json(featurecollection)
-FROM (
-    SELECT
-        'FeatureCollection' AS type,
-        array_to_json(array_agg(features)) AS features
+
+    SELECT row_to_json(featurecollection) INTO geojson
     FROM (
         SELECT
-            row_to_json((SELECT p FROM (SELECT no) p)) AS properties,
-            'Feature' AS type,
-            ST_AsGeoJSON(wktloc)::json AS geometry
-        FROM geom_zones
-    ) features
-) featurecollection
+            'FeatureCollection' AS type,
+            array_to_json(array_agg(features)) AS features
+        FROM (
+            SELECT
+                row_to_json((SELECT p FROM (SELECT no) p)) AS properties,
+                'Feature' AS type,
+                ST_AsGeoJSON(wktloc)::json AS geometry
+            FROM geom_zones
+        ) features
+    ) featurecollection;
+    return geojson;
 
 END;
 $$ LANGUAGE plpgsql;
