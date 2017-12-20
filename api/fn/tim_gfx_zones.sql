a591f647155451656a166b79c9276b423ffadba7
wtsay <wtsay@dvrpc.org>
2017-12-20 13:51:17 -0500

    Zones: GeoJSON (dynamic)

--- api/fn/tim_gfx_zones.sql
+++ api/fn/tim_gfx_zones.sql
@@ -1,23 +1,23 @@
-CREATE OR REPLACE FUNCTION tim_gfx_zones()
+CREATE OR REPLACE FUNCTION tim_gfx_zones(geomtype TEXT)
 RETURNS JSON AS $$
 DECLARE
     geojson JSON;
 BEGIN
-
-    SELECT row_to_json(featurecollection) INTO geojson
+    EXECUTE(FORMAT('
+    SELECT row_to_json(featurecollection)
     FROM (
         SELECT
-            'FeatureCollection' AS type,
+            ''FeatureCollection'' AS type,
             array_to_json(array_agg(features)) AS features
         FROM (
             SELECT
                 row_to_json((SELECT p FROM (SELECT no) p)) AS properties,
-                'Feature' AS type,
-                ST_AsGeoJSON(wktloc)::json AS geometry
+                ''Feature'' AS type,
+                ST_AsGeoJSON(%I, maxdecimaldigits:=5)::json AS geometry
             FROM geom_zones
         ) features
     ) featurecollection;
-    return geojson;
-
+    ', geomtype)) INTO geojson;
+    RETURN geojson;
 END;
 $$ LANGUAGE plpgsql;
--- api/zones.php
+++ api/zones.php
@@ -4,10 +4,24 @@
     $type = ParseType($_GET);
     $atts = ParseAttributes($_GET);
 
-    $_qry = "SELECT COUNT(*) FROM mtx_2000_am WHERE oindex = $1;";
+    // OH BOY OH BOY OH BOY THERE YOU ARE MR. switch
+    switch ($type) {
+        case "gpt":
+            $qry = "SELECT tim_gfx_zones('wktloc')";
+            break;
+        case "gln":
+            $qry = "SELECT tim_gfx_zones('wktpoly')";
+            break;
+        case "gpg":
+            $qry = "SELECT tim_gfx_zones('wktsurface')";
+            break;
+        default:
+            die("Invalid type");
+    }
+
     $con = ConnectToDB();
-    $req = pg_query_params($_qry, array(135)) or
+    $req = pg_query_params($qry, array()) or
         die('Query failed: ' . pg_last_error());
-    $payload = pg_fetch_array($req);
-    echo json_encode($payload);
+    $payload = pg_fetch_row($req);
+    echo $payload[0];
 ?>
