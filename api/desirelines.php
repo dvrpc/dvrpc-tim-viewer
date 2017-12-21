15f6a366186a66aeeb7ccf3fe22e9e0e335c6cf9
wtsay <wtsay@dvrpc.org>
2017-12-21 13:34:36 -0500

    2nd time writing this today

--- api/desirelines.php
+++ api/desirelines.php
@@ -3,27 +3,46 @@
 
     $type = ParseType($_GET);
 
-    $origzoneno = _parseAttribute("oz", $_GET);
-    if (_parseAttribute("dz", $_GET, TRUE)) {
-        $destzonenos = "(SELECT array_agg(DISTINCT(no)) FROM net_zones)";
-    } else {
-        $destzonenos = pg_toIntArray(_parseAttributes("dz", $_GET));
+    function FUCKINGFUCK($key, $array) {
+        $key_cntflag = $key . 'n';
+        switch(_parseAttribute($key_cntflag, $array)) {
+            case -1:
+                return "(SELECT array_agg(no) FROM net_zones)";
+                break;
+            case 0:
+                die("blarg, i'm ded");
+                break;
+            default:
+                return pg_toIntArray(_parseAttributes($key, $array));
+                break;
+        }
     }
+
+    $origzonenos = FUCKINGFUCK("oz", $_GET);
+    $destzonenos = FUCKINGFUCK("dz", $_GET);
+
+    // $origzoneno = _parseAttribute("oz", $_GET);
+    // if (_parseAttribute("dz", $_GET, TRUE)) {
+        // $destzonenos = "(SELECT array_agg(DISTINCT(no)) FROM net_zones)";
+    // } else {
+        // $destzonenos = pg_toIntArray(_parseAttributes("dz", $_GET));
+    // }
     $matrixno = _parseAttribute("m", $_GET);
 
     switch ($type) {
         case "ddl":
-            $qry = "SELECT tim_gfx_ddl($1, $2,  ". $destzonenos .")";
+            $qry = "SELECT tim_gfx_ddl($1, " . $origzonenos . ", ". $destzonenos .")";
             break;
         case "vddl":
-            $qry = "SELECT tim_gfx_vddl($1, $2, ". $destzonenos .")";
+            $qry = "SELECT tim_gfx_vddl($1, " . $origzonenos . ", ". $destzonenos .")";
             break;
         default:
             die("Invalid type");
+            break;
     }
 
     $con = ConnectToDB();
-    $req = pg_query_params($qry, array($matrixno, $origzoneno)) or
+    $req = pg_query_params($qry, array($matrixno)) or
         die('Query failed: ' . pg_last_error());
     $payload = pg_fetch_row($req);
     echo $payload[0];
