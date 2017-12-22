67f2771f785d2ea22fb0f2b2ab53844fbee8986c
wtsay <wtsay@dvrpc.org>
2017-12-22 08:51:42 -0500

    Dynamic Identifier

--- api/fn/tim_buildquery.sql
+++ api/fn/tim_buildquery.sql
@@ -1,16 +1,24 @@
-CREATE OR REPLACE FUNCTION _debug_agg_json(netobj TEXT, fields TEXT[])
+CREATE OR REPLACE FUNCTION _debug_agg_json(netobj TEXT)
 RETURNS TABLE(uid JSON) AS $$
 DECLARE
-    _field_placeholders TEXT[];
+    fields TEXT;
     tablename TEXT;
     f TEXT;
 BEGIN
     tablename := FORMAT('%s', netobj);
+
+    EXECUTE(FORMAT('
+        SELECT array_to_string(array_agg(column_name::text), '','')
+        FROM meta
+        WHERE table_name = %I
+        AND udt_name <> ''geometry''
+    ', tablename)) INTO fields;
+
     FOR f IN (SELECT UNNEST(fields)) LOOP
-	_field_placeholders := array_append(_field_placeholders, FORMAT('%I', f));
+    fields := array_append(fields, FORMAT('%I', f));
     END LOOP;
-    
-    RAISE NOTICE '%', FORMAT('SELECT %s FROM %I', (SELECT array_to_string(_field_placeholders, ',')), tablename);
+
+    RAISE NOTICE '%', FORMAT('SELECT %s FROM %I', fields, tablename);
     RETURN QUERY
     SELECT array_to_json(array_agg(fa)) FROM temp;
 END;
