fdb04ae5f92fb51a94c8954836ad367888288c7f
wtsay <wtsay@dvrpc.org>
2017-12-28 11:17:28 -0500

    GZip to x-zip

--- api/countlocations.php
+++ api/countlocations.php
@@ -1,6 +1,6 @@
 <?php
     include('_functions.php');
-    header('Content-Encoding: gzip');
+    header('Content-Encoding: x-gzip');
     $payload = gzencode(GetGeoJSON("countlocations", $_GET));
     header('Content-Length: ' . strlen($payload));
     echo $payload;
