8bae33b58f185e7f943831487cd629621fd6d182
will <will@wololo.co>
2018-02-14 09:27:09 -0500

    Turn On
    Floating AnarchY

--- api/matrices.php
+++ api/matrices.php
@@ -1,3 +1,10 @@
 <?php
-
+    include("_functions.php");
+    $matrixno = (int) _parseAttribute("m", $_GET);
+    $zones = pg_toIntArray(_parseAttributes("z", $_GET));
+    $con = ConnectToDB();
+    $req = pg_query_params("SELECT tim_mtxvals_json($1," . $zones . ")", array($matrixno)) or
+        die('Query failed: ' . pg_last_error());
+    $payload = pg_fetch_row($req);
+    echo $payload[0];
 ?>
