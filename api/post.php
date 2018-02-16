423ec9fb31cac00e7ac6ff433c8b5131dec5ab6b
will <will@wololo.co>
2018-02-16 09:36:25 -0500

    Welcoming the Dawn
    Kalpataru Tree
    
    NEW ALBUM TODAY WHEN IS IT COMING OUT
    
    ... still bitter over Grouch (NZ)'s allegedly new album that is 'coming
    soon' ... that was 4 months ago. And basically for one song that was
    played in a live set 3 years ago.

--- api/post.php
+++ api/post.php
@@ -5,6 +5,52 @@
         die(json_encode(array('error' => $msg)));
     }
 
+    function CheckNetObj($post) {
+        $qry = "SELECT CASE WHEN $1::TEXT IN (SELECT DISTINCT(netobj) FROM tim_netobj_keys) THEN TRUE ELSE FALSE END netobjfound";
+        $netobj = NULL;
+        if (array_key_exists("netobj", $post)) {
+            $netobj = $post["netobj"];
+        } else {
+            kill("Missing required key 'netobj'");
+        }
+        $con = ConnectToDB();
+        $req = pg_query_params($qry, array($netobj)) or kill('DB Error');
+        if (pg_fetch_result($req, 0, 'netobjfound') == 't') {
+            return $netobj;
+        } else {
+            return NULL;
+        }
+    }
+
+    function CheckNetObjKeys($post) {
+        // Implicit valid "netobj" for $post
+        $netobj = $post["netobj"];
+        $qry = "SELECT field FROM tim_netobj_keys WHERE netobj = $1::TEXT";
+        if (array_key_exists("keys", $post)) {
+            if (is_array($post["keys"])) {
+                
+            } else {
+                kill("Invalid value for key 'keys'");
+            }
+        } else {
+            kill("Missing required key 'keys'");
+        }
+        $req = pg_query_params($qry, array($netobj)) or kill('DB Error');
+        $payload = pg_fetch_assoc($req);
+        $keys = array();
+        foreach($payload as $_ => $key) {
+            if (array_key_exists($key, $post["keys"])) {
+                $value = $post["keys"][$key];
+                $keys[$key] = $value;
+            } else {
+                die("Missing required netobj key '" . $key . "'");
+            }
+        }
+        return $keys;
+    }
+
+    ////
+
     if (strcasecmp($_SERVER['REQUEST_METHOD'], 'GET') == 0) {
         kill('GET');
     } else if (strcasecmp($_SERVER['REQUEST_METHOD'], 'POST') == 0) {
@@ -18,29 +64,24 @@
         kill(":(");
     }
 
-    $con = ConnectToDB();
-    $netobj = NULL;
+    ////
 
-    $SQL_GET_KEYS = "SELECT field FROM tim_netobj_keys WHERE netobj = $1::TEXT";
-
-    if (array_key_exists("netobj", $post)) {
-        $netobj = $post["netobj"];
-    } else {
-        kill("Missing required key 'netobj'");
-    }
-
-    if (array_key_exists("keys", $post)) {
+    $netobj = CheckNetObj($post);
+    if ($netobj) {
         
     } else {
-        kill("Missing required key 'keys'");
+        kill("Invalid netobj '" . $post["netobj"] . "'");
     }
 
-    $req = pg_query_params($SQL_GET_KEYS, array($netobj)) or kill('DB Error');
-    $payload = pg_fetch_array($req);
+    ////
+
+    $netobjkeys = CheckNetObjKeys($post);
+
+    ////
 
     die(json_encode(array(
         'netobj' => $netobj,
-        'dbkeys' => $payload
+        'netobjkeys' => $netobjkeys
     )));
 
 ?>
