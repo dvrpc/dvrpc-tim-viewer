10dd6b29f3d99fc8c60f2d8fd1f266bc080736e5
will <will@wololo.co>
2018-02-20 09:40:31 -0500

    Delay figuring out how Lighttpd works

---
+++ api/mainturn.php
@@ -0,0 +1,16 @@
+<?php
+    include("_functions.php");
+    $netobj = "mainturn";
+
+    header('Content-Type: application/json');
+    header('Content-Encoding: x-gzip');
+    $response = Operator(
+        $netobj,
+        $_SERVER['REQUEST_METHOD'],
+        $_GET,
+        trim(file_get_contents("php://input"))
+    );
+    $payload = gzencode($response);
+    header('Content-Length: ' . strlen($payload));
+    echo $payload;
+?>
