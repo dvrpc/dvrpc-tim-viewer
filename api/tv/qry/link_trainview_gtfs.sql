8bea33bc995035a4cc619d81b5da94eae9fc6773
wtsay <wtsay@dvrpc.org>
2018-04-04 12:06:21 -0400

    Fully Link Trainview to GTFS
    Test case runs in ~2s

--- api/tv/qry/link_trainview_gtfs.sql
+++ api/tv/qry/link_trainview_gtfs.sql
@@ -56,22 +56,29 @@ ON tvd.trainno = gtfst.trip_short_name
 -- Def fastest tho
 _shapes AS (
     SELECT _shapes.shape_id,
-    ST_MakeLine(array_agg(ST_SetSRID(ST_MakePoint(shape_pt_lon, shape_pt_lat), 4326)))
-    FROM (SELECT DISTINCT(gtfs_id) FROM trainview_gtfs_day_ids('2018-02-14')) _gtfs_ids
+    ST_MakeLine(array_agg(ST_SetSRID(ST_MakePoint(shape_pt_lon, shape_pt_lat), 4326))) geom
+    FROM (SELECT DISTINCT(gtfs_id) FROM trainview_gtfs_day_ids('2018-02-05 00:00:00')) _gtfs_ids
     LEFT JOIN gtfs_shapes _shapes
     ON _gtfs_ids.gtfs_id = _shapes.gtfs_id
     GROUP BY _shapes.shape_id
+),
+_full_lines AS (
+    SELECT shape_ids, ST_LineMerge(ST_Collect(geom)) geom
+    FROM (SELECT DISTINCT(shape_ids) FROM _ultra) _u
+    LEFT JOIN _shapes
+    ON _shapes.shape_id = ANY(shape_ids)
+    GROUP BY shape_ids
 )
 SELECT
     -- ARRAY[lat, lon]::REAL[]
     timestmp,
     line,
     late,
-    lat, lon,
-    shape_ids
+    ST_ShortestLine(ST_SetSRID(ST_MakePoint(lon, lat), 4326), _full_lines.geom),
+    _ultra.shape_ids
 FROM _ultra
-LEFT JOIN _shapes
-ON _shapes.shape_id = ANY(shape_ids)
+LEFT JOIN _full_lines
+ON _full_lines.shape_ids = _ultra.shape_ids
 WHERE
     timestmp > (astart_time - '1 hour'::interval)
 AND timestmp < (aend_time + '4 hours'::interval)
