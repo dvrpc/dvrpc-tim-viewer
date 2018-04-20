fe83bc5f87b15ccb15977e4dbe9a5fc3f2e4d5bd
wtsay <wtsay@dvrpc.org>
2018-04-20 11:10:46 -0400

    Zones Using Stop Point

---
+++ api/qry/put_stoppoint_zones.sql
@@ -0,0 +1,28 @@
+WITH odpairs AS (
+    SELECT ozoneno, dzoneno, pathindex FROM putpathlegs 
+    WHERE
+        scen = '2015'
+    AND tod = 'AM'
+    AND fromstoppointno = ANY(ARRAY[101927]::INTEGER[])
+    GROUP BY ozoneno, dzoneno, pathindex
+    ORDER BY ozoneno, dzoneno
+),
+ppl AS (
+    SELECT ppl.* FROM putpathlegs ppl
+    INNER JOIN odpairs odp
+    ON ppl.ozoneno = odp.ozoneno
+    AND ppl.dzoneno = odp.dzoneno
+    AND ppl.pathindex = odp.pathindex
+),
+_t AS (
+    SELECT
+    ozoneno
+    ,dzoneno
+    ,pathindex
+    ,odtrips
+    -- ,array_agg(array[fromstoppointno, tostoppointno])
+    FROM ppl
+    GROUP BY ozoneno, dzoneno, pathindex, odtrips
+)
+SELECT * FROM _t
+ORDER BY odtrips DESC
