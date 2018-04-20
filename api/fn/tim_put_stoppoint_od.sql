97084d4e6805ee55a73d28fccd13605378361a29
wtsay <wtsay@dvrpc.org>
2018-04-20 14:05:15 -0400

    PPL: Get ODPI list for stoppointno(s)

---
+++ api/fn/tim_put_stoppoint_od.sql
@@ -0,0 +1,37 @@
+DROP FUNCTION IF EXISTS tim_put_stoppoint_od(INTEGER[], TEXT, TEXT);
+DROP FUNCTION IF EXISTS tim_put_stoppoint_od(INTEGER, TEXT, TEXT);
+
+CREATE OR REPLACE FUNCTION tim_put_stoppoint_od(stoppoints INTEGER[], scenario TEXT, timeofday TEXT)
+RETURNS TABLE (
+    ozoneno INTEGER,
+    dzoneno INTEGER,
+    pathindex INTEGER
+) AS $$
+BEGIN
+    RETURN QUERY
+        SELECT
+            ppl.ozoneno,
+            ppl.dzoneno,
+            ppl.pathindex
+        FROM putpathlegs ppl
+        WHERE
+            ppl.scen = scenario
+        AND ppl.tod = timeofday
+        -- (to|from)stoppointno are offset from each other by one with a (trailing|leading) null
+        AND ppl.fromstoppointno = ANY(stoppoints)
+        GROUP BY ppl.ozoneno, ppl.dzoneno, ppl.pathindex
+        ORDER BY ppl.ozoneno, ppl.dzoneno, ppl.pathindex;
+END;
+$$ LANGUAGE plpgsql;
+
+CREATE OR REPLACE FUNCTION tim_put_stoppoint_od(stoppoint INTEGER, scenario TEXT, timeofday TEXT)
+RETURNS TABLE (
+    odzoneno INTEGER,
+    dzoneno INTEGER,
+    pathindex INTEGER
+) AS $$
+BEGIN
+    RETURN QUERY
+    SELECT * FROM tim_put_stoppoint_od(ARRAY[stoppoint]::INTEGER[], scenario, timeofday);
+END;
+$$ LANGUAGE plpgsql;
