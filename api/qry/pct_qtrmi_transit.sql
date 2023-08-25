d1788d77d0eccc93649e8193defda13cc962b85e
wtsay <will@wololo.co>
2023-08-25 12:37:56 -0400

    Update Pct QtrMi Transit
    Fixed weird issue with 100% overlapping geometries

--- api/qry/pct_qtrmi_transit.sql
+++ api/qry/pct_qtrmi_transit.sql
@@ -1,9 +1,9 @@
 -- Buffer Stop Points, Intersect with Zones
 DROP TABLE IF EXISTS _temp_geom_zones;
 DROP TABLE IF EXISTS _temp_geom_stoppoints;
-
 CREATE TABLE IF NOT EXISTS _temp_geom_zones AS
-    SELECT no, ST_MakeValid(ST_Transform(wktsurface, 26918)) wktsurface FROM geom_zones;
+    SELECT no, ST_MakeValid(ST_Transform(wktsurface, 26918)) wktsurface FROM geom_zones
+    WHERE ST_Area(wktsurface) > 0;
 CREATE INDEX IF NOT EXISTS _temp_geom_zones_gidx ON _temp_geom_zones USING GIST(wktsurface);
 CREATE TABLE IF NOT EXISTS _temp_geom_stoppoints AS
     SELECT ST_MakeValid(ST_Union(wktloc)) geom FROM (
@@ -12,14 +12,27 @@ CREATE TABLE IF NOT EXISTS _temp_geom_stoppoints AS
     ) q;
 CREATE INDEX IF NOT EXISTS _temp_geom_stoppoints_gidx ON _temp_geom_stoppoints USING GIST(geom);
 
-SELECT gz.no,
-SUM(COALESCE((ST_Area(ST_Intersection(gz.wktsurface, gsp.geom))/ST_Area(gz.wktsurface))*100,0)) pct
-FROM _temp_geom_zones gz
-LEFT JOIN _temp_geom_stoppoints gsp
-ON ST_Overlaps(gz.wktsurface, gsp.geom) OR ST_Overlaps(gsp.geom, gz.wktsurface)
-GROUP BY gz.no
-ORDER BY pct DESC
-LIMIT 1000
+ALTER TABLE net_zones DROP COLUMN IF EXISTS pct_qtrmi_transit;
+ALTER TABLE net_zones ADD COLUMN IF NOT EXISTS pct_qtrmi_transit DOUBLE PRECISION;
+
+UPDATE net_zones 
+SET pct_qtrmi_transit = q.pct
+FROM (
+    SELECT 
+        gz.no,
+        SUM(COALESCE(
+            (ST_Area(ST_Intersection(gz.wktsurface, gsp.geom))/ST_Area(gz.wktsurface)),
+        0)) pct
+    FROM _temp_geom_zones gz
+    LEFT JOIN _temp_geom_stoppoints gsp
+    ON TRUE
+    -- ON ST_Overlaps(gz.wktsurface, gsp.geom) OR ST_Overlaps(gsp.geom, gz.wktsurface)
+    GROUP BY gz.no
+) q
+WHERE net_zones.no = q.no;
+
+DROP TABLE IF EXISTS _temp_geom_zones;
+DROP TABLE IF EXISTS _temp_geom_stoppoints;
 
 -- Buffer Zones, Intersect with Stop Points
 DROP TABLE IF EXISTS _temp_geom_zones_26918;
